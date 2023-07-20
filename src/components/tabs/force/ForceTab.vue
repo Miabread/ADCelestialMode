2bfda465d67343cfe84bb4969e4dbd6e37c2230a
Miabread <jamesbeeprog@gmail.com>
2023-07-20 19:32:35 -0400

    Implement ForceTab

--- src/components/tabs/force/ForceTab.vue
+++ src/components/tabs/force/ForceTab.vue
@@ -1,11 +1,44 @@
 <script>
+import PrimaryToggleButton from "@/components/PrimaryToggleButton";
+
 export default {
   name: "ForceTab",
+  components: {
+    PrimaryToggleButton
+  },
+  data() {
+    return { force: JSON.parse(JSON.stringify(player.force)) };
+  },
+  methods: {
+    update() {
+      player.force = JSON.parse(JSON.stringify(this.force));
+    }
+  }
 };
 </script>
 
 <template>
-  <h1>Force</h1>
+  <div>
+    <div v-for="(subtopics, topic) in force">
+      <h1>{{ topic }}</h1>
+      <div v-for="(value, key) in subtopics">
+        <PrimaryToggleButton
+            v-if="(typeof value !== 'object')"
+           :label="key" 
+           :value="force[topic][key]" 
+           @input="force[topic][key] = $event"/>
+        <template v-else>
+          <h2>{{ key }}</h2>
+          <div v-for="(value, subkey) in value">
+            <PrimaryToggleButton
+            :label="subkey" 
+            :value="force[topic][key][subkey]" 
+            @input="force[topic][key][subkey] = $event"/>
+          </div>
+        </template>
+      </div>
+    </div>
+  </div>
 </template>
 
 <style scoped>
